37144a5d2036b067d2c8fe404f14d534fad948fc
almahmoud <mahmoudalexandru@gmail.com>
2022-01-27 17:25:18 -0500

    indentation

--- devel_install.sh
+++ devel_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if ! $(helm repo ls | grep -q bioc);
-then
-helm repo add bioc https://github.com/Bioconductor/helm-charts/raw/devel; fi
+  then helm repo add bioc https://github.com/Bioconductor/helm-charts/raw/devel
+fi
 
 
 
@@ -12,30 +12,30 @@ do
         n) namespace=${OPTARG};;
         s) size=${OPTARG};;
         t) tag=${OPTARG};;
-	r) repository=${OTPARG};;
+    r) repository=${OTPARG};;
     esac
 done
 
 if [ -z "$namespace" ];
-	then echo "A namespace must be specific with eg: -n myinitials-mynamespace";
-	exit;
+    then echo "A namespace must be specific with eg: -n myinitials-mynamespace";
+    exit;
 fi
 
 
 if [ -z "$size" ];
-	then echo "A disk size must be specific with eg: -s 10[Gi|Mi]";
-	exit;
+    then echo "A disk size must be specific with eg: -s 10[Gi|Mi]";
+    exit;
 fi
 
 
 if [ -z "$tag" ];
-	# Set default tag
-	export tag="devel";
+    # Set default tag
+    export tag="devel";
 fi
 
 if [ -z "$repository" ];
-	# Set default repo
-	export repository="bioconductor/bioconductor_docker";
+    # Set default repo
+    export repository="bioconductor/bioconductor_docker";
 fi
 
 helm upgrade --create-namespace --install -n "$namespace" bioc-script bioc/bioconductor \
